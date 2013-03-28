f638622acf8c5e41c025430e30c905359a4450a1
Edward Laird <elaird@gmail.com>
2013-03-28 04:10:28 -0500

    whitespace

--- configuration.py
+++ configuration.py
@@ -1,7 +1,9 @@
 from supy.defaults import *
 
-def mainTree() :
-    return ("susyTree","tree")
 
-def leavesToBlackList() :
+def mainTree():
+    return ("susyTree", "tree")
+
+
+def leavesToBlackList():
     return ['L1triggered', 'triggered', 'DCSBits']
